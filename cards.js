abf8b5cff25fc00c3ac082e814bf7e7af8e9ce30
aoanbe-ui <ao.anbe@gmail.com>
2026-04-29 23:47:20 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -85,3 +85,29 @@ function renderCards(cardsArray) {
 
 // Call the function
 renderCards(projectCards);
+
+// --- REDUCE ---
+// Goal: Total character count of all titles
+const totalTitleChars = projectCards.reduce((acc, card) => {
+    return acc + card.title.length;
+}, 0);
+console.log("Total Title Characters:", totalTitleChars);
+
+// --- SORT ---
+// Goal: Sort alphabetically by title
+const sortedCards = [...projectCards].sort((a, b) => {
+    return a.title.localeCompare(b.title);
+});
+console.log("Sorted Cards:", sortedCards);
+
+// --- FILTER ---
+// Goal: Find descriptions containing "JavaScript"
+const filteredCards = projectCards.filter(card => {
+    return card.description.toLowerCase().includes("javascript");
+});
+console.log("Filtered Cards (JavaScript):", filteredCards);
+
+// --- MAP ---
+// Goal: Create an array of just the titles
+const cardTitles = projectCards.map(card => card.title);
+console.log("Card Titles Array:", cardTitles);
